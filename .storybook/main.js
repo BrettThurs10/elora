367a2b297bb064fd0395cdb62f3ba644e0ce68eb
thurs10dev <brettmarcthurston@gmail.com>
2021-09-02 16:53:02 -0500

    customize storybook to work with Tailwind and add stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,26 @@
+const path = require('path')
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            ident: 'postcss',
+            plugins: [require('tailwindcss'), require('autoprefixer')]
+          }
+        }
+      ],
+      include: path.resolve(__dirname, '../')
+    })
+    return config
+  }
 }
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,37 +1,70 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-
-import { Button } from './Button';
+import React, { ComponentProps } from 'react'
+import { DownloadIcon } from '@heroicons/react/outline'
+import { Story, Meta } from '@storybook/react'
+import { Button, buttonStyles } from '../index'
 
 export default {
-  title: 'Example/Button',
-  component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-} as ComponentMeta<typeof Button>;
+  title: 'Button',
+  component: Button
+} as Meta
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+//👇 We create a “template” of how args map to rendering
+const Template: Story<ComponentProps<typeof Button>> = (args) => (
+  <Button {...args} onClick={() => alert('boop')}>
+    <p>Click me</p>
+  </Button>
+)
 
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: 'Button',
-};
+export const SimpleButton = Template.bind({})
+SimpleButton.args = {
+  buttonStyle: 'simple'
+}
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
+export const PillButton = Template.bind({})
+PillButton.args = {
+  buttonStyle: 'pill'
+}
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
+export const OutlineButton = Template.bind({})
+OutlineButton.args = {
+  buttonStyle: 'outline'
+}
 
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+export const BorderedButton = Template.bind({})
+BorderedButton.args = {
+  buttonStyle: 'bordered'
+}
+
+export const ElevatedButton = Template.bind({})
+ElevatedButton.args = {
+  buttonStyle: 'elevated'
+}
+
+export const ThreeDButton = Template.bind({})
+ThreeDButton.args = {
+  buttonStyle: '3d'
+}
+
+export const DisabledButton = Template.bind({})
+DisabledButton.args = {
+  buttonStyle: 'disabled'
+}
+
+export const IconButton = Template.bind({})
+IconButton.args = {
+  buttonStyle: 'icon',
+  icon: <DownloadIcon className={buttonStyles.actualIcon()} />
+}
+
+export const ColorOverride = Template.bind({})
+ColorOverride.args = {
+  buttonStyle: 'simple',
+  customColor: 'green'
+}
+
+export const ClassNameColorOverride = Template.bind({})
+ClassNameColorOverride.args = {
+  buttonStyle: 'simple',
+  customColor: 'pink',
+  classNameOverride: buttonStyles.anotherButton
+}
