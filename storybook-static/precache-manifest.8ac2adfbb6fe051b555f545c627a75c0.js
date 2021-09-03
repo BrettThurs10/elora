0c3a307acfe3f482bd72f0b872eeb3c21f92320c
thurs10dev <brettmarcthurston@gmail.com>
2021-09-03 10:47:44 -0500

    add storybook static for github page

---
+++ storybook-static/precache-manifest.8ac2adfbb6fe051b555f545c627a75c0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7396ab4dfe565145e86d",
+    "url": "0.fc5606cd.iframe.bundle.js"
+  },
+  {
+    "revision": "c6d7426be09917d9abed",
+    "url": "4.f61b5785.iframe.bundle.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "4.f61b5785.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "472fafbca061e04764fc",
+    "url": "5.cc182f6f.iframe.bundle.js"
+  },
+  {
+    "revision": "4ec3f9680718f41c6184",
+    "url": "6.57822e0c.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "6.57822e0c.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "a61b24bcde6c61e66f26",
+    "url": "7.f49705ad.iframe.bundle.js"
+  },
+  {
+    "revision": "81be4a6b3fcaba0c7a54ad15540e8ab9",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "f2bbd8ae46e4bf18a279",
+    "url": "main.31b39ca0.iframe.bundle.js"
+  },
+  {
+    "revision": "79cb51057194cc15d5ba",
+    "url": "runtime~main.23366c2f.iframe.bundle.js"
+  },
+  {
+    "revision": "f2bbd8ae46e4bf18a279",
+    "url": "static/css/main.fa49afd0.chunk.css"
+  },
+  {
+    "revision": "e8a75672ec9dc1118dc1",
+    "url": "vendors~main.d1d82629.iframe.bundle.js"
+  },
+  {
+    "revision": "25659aa5b361e026cba8b02c899cd6f0",
+    "url": "vendors~main.d1d82629.iframe.bundle.js.LICENSE.txt"
+  }
+]);
