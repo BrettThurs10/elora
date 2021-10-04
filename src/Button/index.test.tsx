de504c51c0c0e5208b7d9c07f71eb1876e207a5b
thurs10dev <brettmarcthurston@gmail.com>
2021-10-04 13:27:37 -0500

    add more RTL tests for button component

--- src/Button/index.test.tsx
+++ src/Button/index.test.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
-import { screen, render } from '@testing-library/react'
+import { screen, render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Button from './index'
 
-test('Button renders children', async () => {
-    render(
-        <Button>
-            <p>Click me</p>
-        </Button>
-    )
-    expect(screen.getByText('Click me'))
+describe('<Button/>', () => {
+    it('renders children', async () => {
+        render(
+            <Button>
+                <p>Click me</p>
+            </Button>
+        )
+        expect(screen.getByText('Click me'))
+    })
+    it('onClick executes', async () => {
+        const mockFn = jest.fn()
+        render(
+            <Button data-testid='btn-click' onClick={mockFn}>
+                <p>Click me</p>
+            </Button>
+        )
+        const btnClick = screen.getByTestId('btn-click')
+        fireEvent.click(btnClick)
+        expect(mockFn).toHaveBeenCalled()
+    })
 })
